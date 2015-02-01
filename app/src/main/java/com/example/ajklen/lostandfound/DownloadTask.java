71d74f1e1e2b22532ff722d1427ef0945dbc9e23
ajklen <adam.j.klen@gmail.com>
2015-02-01 01:58:38 -0500

    Cleaned up DownloadTask class and stuff.

---
+++ app/src/main/java/com/example/ajklen/lostandfound/DownloadTask.java
@@ -0,0 +1,88 @@
+package com.example.ajklen.lostandfound;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Implementation of AsyncTask, to fetch the data in the background away from
+ * the UI thread.
+ */
+public class DownloadTask extends AsyncTask<String, Void, String> {
+    OnTaskCompleted listener;
+    public DownloadTask(OnTaskCompleted listener){
+        this.listener=listener;
+    }
+
+    protected void onPostExecute(String r){
+        listener.callback(r);
+    }
+
+    @Override
+    protected String doInBackground(String... urls) {
+        try {
+            return loadFromNetwork(urls[0]);
+        } catch (IOException e) {
+            return "Connection error.";
+        }
+    }
+
+    /** Initiates the fetch operation. */
+    private String loadFromNetwork(String urlString) throws IOException {
+        InputStream stream = null;
+        String str ="";
+
+        try {
+            stream = downloadUrl(urlString);
+            str = readIt(stream, 400);
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+        return str;
+    }
+
+    /**
+     * Given a string representation of a URL, sets up a connection and gets
+     * an input stream.
+     * @param urlString A string representation of a URL.
+     * @return An InputStream retrieved from a successful HttpURLConnection.
+     * @throws java.io.IOException
+     */
+    private InputStream downloadUrl(String urlString) throws IOException {
+        // BEGIN_INCLUDE(get_inputstream)
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Start the query
+        conn.connect();
+        InputStream stream = conn.getInputStream();
+        return stream;
+        // END_INCLUDE(get_inputstream)
+    }
+
+    /** Reads an InputStream and converts it to a String.
+     * @param stream InputStream containing HTML from targeted site.
+     * @param len Length of string that this method returns.
+     * @return String concatenated according to len parameter.
+     * @throws java.io.IOException
+     * @throws java.io.UnsupportedEncodingException
+     */
+    private String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+}
---
+++ app/src/main/java/com/example/ajklen/lostandfound/OnTaskCompleted.java
@@ -0,0 +1,8 @@
+package com.example.ajklen.lostandfound;
+
+/**
+ * Created by ajklen on 2/1/15.
+ */
+interface OnTaskCompleted {
+    public void callback(String result);
+}
