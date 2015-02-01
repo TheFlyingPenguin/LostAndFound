e2a6d3900631c9fedcaca4cc6b75543b13d16bf8
ajklen <adam.j.klen@gmail.com>
2015-02-01 01:04:19 -0500

    Bare minimum sliding tabs display.

--- app/src/main/java/com/example/ajklen/lostandfound/ContentFragment.java
+++ app/src/main/java/com/example/ajklen/lostandfound/ContentFragment.java
@@ -16,7 +16,6 @@
 
 package com.example.ajklen.lostandfound;
 
-import com.example.ajklen.lostandfound.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
